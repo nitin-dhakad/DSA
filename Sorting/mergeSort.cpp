de19e6ff200db2734e4746ed96bb4c68f4beb3ec
Nitin Dhakad <nitin.dhakad@nagarro.com>
2022-06-02 14:15:08 +0530

    git ignore added

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-void *merge(int arr[], int left, int mid, int right)
+void merge(int arr[], int left, int mid, int right)
 {
     int n1 = mid - left + 1;
     int n2 = right - mid;
